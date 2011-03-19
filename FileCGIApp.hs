1fcbb222e9e5d1651a900e2d164794a20cf6d66c
Kazu Yamamoto <kazu@iij.ad.jp>
2011-03-19 09:24:49 +0900

    initial logger.

--- FileCGIApp.hs
+++ FileCGIApp.hs
@@ -3,12 +3,15 @@
 module FileCGIApp (fileCgiApp) where
 
 import Config
-import Data.ByteString.Char8 (ByteString, isPrefixOf, pack)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as BS
 import Data.List (isSuffixOf)
 import Network.Wai
 import Network.Wai.Application.CGI
 import Network.Wai.Application.File
 import Types
+import Data.Time
+import System.Locale
 
 fileCgiApp :: Option -> URLMap -> Application
 fileCgiApp opt um req = case mmp of
@@ -21,9 +24,10 @@ fileCgiApp opt um req = case mmp of
   where
     mmp = getBlock (serverName req) um >>= getMapper (pathInfo req)
     spec = AppSpec {
-        softwareName = pack $ opt_server_name opt
+        softwareName = BS.pack $ opt_server_name opt
       , indexFile = opt_index_file opt
       , isHTML = \x -> ".html" `isSuffixOf` x || ".htm" `isSuffixOf` x
+      , logger = mightyLogger
       }
 
 getBlock :: ByteString -> URLMap -> Maybe [Mapper]
@@ -35,5 +39,23 @@ getBlock key (Block doms maps : ms)
 getMapper :: ByteString -> [Mapper] -> Maybe Mapper
 getMapper _ [] = Nothing
 getMapper key (m@(Mapper src _ _):ms)
-  | src `isPrefixOf` key = Just m
-  | otherwise            = getMapper key ms
+  | src `BS.isPrefixOf` key = Just m
+  | otherwise               = getMapper key ms
+
+mightyLogger :: Request -> RspSpec -> IO ()
+mightyLogger req rsp = do
+    (getPeerAddr $ remoteHost req) >>= putStr
+    BS.putStr " - - ["
+    zt <- getZonedTime
+    putStr $ formatTime defaultTimeLocale "%d/%b/%Y:%T %z" zt
+    BS.putStr "] \""
+    BS.putStr $ requestMethod req
+    BS.putStr " "
+    BS.putStr $ pathInfo req
+    BS.putStr "\" "
+    putStr . show . statusCode . rspStatus $ rsp
+    BS.putStr " - \"" -- size
+    BS.putStr $ lookupRequestField' fkReferer req
+    BS.putStr "\" \""
+    BS.putStr $ lookupRequestField' fkUserAgent req
+    BS.putStr "\"\n"
