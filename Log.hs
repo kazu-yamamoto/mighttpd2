1a14a2f80db1bb8cca43d71b1cd06cdb1e5a1851
Kazu Yamamoto <kazu@iij.ad.jp>
2011-07-07 17:56:20 +0900

    Using new getPeerAddr.

--- Log.hs
+++ Log.hs
@@ -71,7 +71,7 @@ rotate spec = mapM_ move srcdsts
 
 apacheLogger :: TimeRef -> HandleRef -> Request -> Status -> Maybe Integer -> IO ()
 apacheLogger timref hdlref req st msize = do
-    addr <- getPeerAddr (remoteHost req)
+    let addr = getPeerAddr (remoteHost req)
     tmstr <- getDate timref
     hdl <- getHandle hdlref
     BS.hPut hdl $ BS.concat [
