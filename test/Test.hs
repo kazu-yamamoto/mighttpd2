c6a5795a7b1f8a98508645f359e287c7ae85b307
Kazu Yamamoto <kazu@iij.ad.jp>
2012-02-07 17:54:14 +0900

    adding test.

--- test/Test.hs
+++ test/Test.hs
@@ -1,15 +1,11 @@
 {-# LANGUAGE OverloadedStrings, TemplateHaskell #-}
 
-{-
-  % runghc -i.. Test.hs
--}
-
-module Test where
+module Main where
 
 import Config.Internal
 import Route
 import Test.Framework.Providers.HUnit
-import Test.Framework.TH
+import Test.Framework.TH.Prime
 import Test.HUnit
 import Types
 
@@ -24,32 +20,14 @@ case_config :: Assertion
 case_config = do
     res <- parseConfig "sample.conf"
     res @?= ans
- where
-    ans = [("Port",CV_Int 80)
-          ,("Debug_Mode",CV_Bool True)
-          ,("User",CV_String "nobody")
-          ,("Group",CV_String "nobody")
-          ,("Pid_File",CV_String "/var/run/mighty.pid")
-          ,("Logging",CV_Bool True)
-          ,("Log_File",CV_String "/var/log/mighty")
-          ,("Log_File_Size",CV_Int 16777216)
-          ,("Log_Backup_Number",CV_Int 10)
-          ,("Index_File",CV_String "index.html")
-          ,("Connection_Timeout",CV_Int 30)
-          ,("Worker_Processes",CV_Int 1)]
+  where
+    ans = [("Port",CV_Int 80),("Debug_Mode",CV_Bool True),("User",CV_String "nobody"),("Group",CV_String "nobody"),("Pid_File",CV_String "/var/run/mighty.pid"),("Logging",CV_Bool True),("Log_File",CV_String "/var/log/mighty"),("Log_File_Size",CV_Int 16777216),("Log_Backup_Number",CV_Int 10),("Index_File",CV_String "index.html"),("Status_File_Dir",CV_String "/usr/local/share/mighty/status"),("Connection_Timeout",CV_Int 30),("Worker_Processes",CV_Int 1)]
 
 case_route :: Assertion
 case_route = do
     res <- parseRoute "sample.route"
     res @?= ans
- where
-    ans = [Block
-           ["localhost","www.example.com"]
-           [RouteCGI "/~alice/cgi-bin/" "/home/alice/public_html/cgi-bin/"
-           ,RouteFile "/~alice/" "/home/alice/public_html/"
-           ,RouteCGI "/cgi-bin/" "/export/cgi-bin/"
-           ,RouteRevProxy "/app/wiki" "/" "localhost" 8080
-           ,RouteRevProxy "/app/cal" "/path" "example.net" 80
-           ,RouteFile "/" "/export/www/"]]
+  where
+    ans = [Block ["localhost","www.example.com"] [RouteCGI "/~alice/cgi-bin/" "/home/alice/public_html/cgi-bin/",RouteFile "/~alice/" "/home/alice/public_html/",RouteCGI "/cgi-bin/" "/export/cgi-bin/",RouteRevProxy "/app/cal" "/calendar" "example.net" 80,RouteRevProxy "/app/wiki" "/app/wiki" "localhost" 8080,RouteFile "/" "/export/www/"]]
 
 ----------------------------------------------------------------
