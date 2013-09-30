b8e439c9217cb8ac2940b7b63573853e5cf70959
Kazu Yamamoto <kazu@iij.ad.jp>
2013-09-30 10:10:54 +0900

    temprary commit.

--- src/Mighty.hs
+++ src/Mighty.hs
@@ -2,19 +2,14 @@
 
 module Main where
 
-import Control.Concurrent (forkIO, threadDelay)
-import Control.Monad (void, unless, when)
+import Control.Monad (when)
 import Data.Version (showVersion)
 import Network.Wai.Application.Classic hiding ((</>), (+++))
-import Network.Wai.Logger (IPAddrSource(FromSocket))
 import System.Directory (getCurrentDirectory)
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
-import System.Exit (exitSuccess)
 import System.FilePath (addTrailingPathSeparator, isAbsolute, normalise, (</>))
 import System.IO
-import System.Posix (getProcessID, setFileMode)
-import System.Log.FastLogger hiding (Logger)
 
 import Program.Mighty
 
@@ -29,12 +24,6 @@ programName = "Mighttpd"
 programVersion :: String
 programVersion = showVersion P.version
 
-backlogNumber :: Int
-backlogNumber = 2048
-
-openFileNumber :: Integer
-openFileNumber = 10000
-
 ----------------------------------------------------------------
 
 main :: IO ()
@@ -44,9 +33,9 @@ main = do
     let reportFile = reportFileName opt
     rpt <- initReporter reportFile >>= checkReporter reportFile
     if opt_debug_mode opt then
-        run opt route rpt
+        server opt route rpt
       else
-        background opt $ run opt route rpt
+        background opt $ server opt route rpt
   where
     getOptRoute = getArgs >>= eachCase
     svrnm = programName ++ "/" ++ programVersion
@@ -98,59 +87,6 @@ main = do
 
 ----------------------------------------------------------------
 
-run :: Option -> RouteDB -> Reporter -> IO ()
-run opt route rpt = reportDo rpt $ do
-    unlimit openFileNumber
-    service <- openService opt
-    unless debug writePidFile
-    logCheck logtype
-    stt <- initStater
-    lgr <- initLogger FromSocket logtype
-    -- killed by signal
-    void . forkIO $ server opt route service rpt stt lgr
-    mainLoop rpt stt lgr
-  where
-    debug = opt_debug_mode opt
-    pidfile = opt_pid_file opt
-    writePidFile = do
-        pid <- getProcessID
-        writeFile pidfile $ show pid ++ "\n"
-        setFileMode pidfile 0o644
-    logspec = FileLogSpec {
-        log_file          = opt_log_file opt
-      , log_file_size     = fromIntegral $ opt_log_file_size opt
-      , log_backup_number = opt_log_backup_number opt
-      }
-    logtype
-      | not (opt_logging opt) = LogNone
-      | debug                 = LogStdout
-      | otherwise             = LogFile logspec
-
-openService :: Option -> IO Service
-openService opt
-  | service == 1 = do
-      s <- listenSocket httpsPort backlogNumber
-      debugMessage $ "HTTP/TLS service on port " ++ httpsPort ++ "."
-      return $ HttpsOnly s
-  | service == 2 = do
-      s1 <- listenSocket httpPort backlogNumber
-      s2 <- listenSocket httpsPort backlogNumber
-      debugMessage $ "HTTP service on port " ++ httpPort ++ " and "
-                  ++ "HTTP/TLS service on port " ++ httpsPort ++ "."
-      return $ HttpAndHttps s1 s2
-  | otherwise = do
-      s <- listenSocket httpPort backlogNumber
-      debugMessage $ "HTTP service on port " ++ httpPort ++ "."
-      return $ HttpOnly s
-  where
-    httpPort  = show $ opt_port opt
-    httpsPort = show $ opt_tls_port opt
-    service = opt_service opt
-    debug = opt_debug_mode opt
-    debugMessage msg = when debug $ do
-        putStrLn msg
-        hFlush stdout
-
 background :: Option -> IO () -> IO ()
 background opt svr = do
     putStrLn $ "Serving on port " ++ show port ++ " and detaching this terminal..."
@@ -159,19 +95,3 @@ background opt svr = do
     daemonize svr
   where
     port = opt_port opt
-
-----------------------------------------------------------------
-
--- FIXME log controller should be implemented here
-mainLoop :: Reporter -> Stater -> Logger -> IO ()
-mainLoop rpt stt lgr = do
-    threadDelay 1000000
-    retiring <- isRetiring stt
-    counter <- getConnectionCounter stt
-    if retiring && counter == 0 then do
-        report rpt "Worker Mighty retired"
-        finReporter rpt
-        finLogger lgr
-        exitSuccess
-      else
-        mainLoop rpt stt lgr
--- src/Server.hs
+++ src/Server.hs
@@ -1,25 +1,21 @@
 {-# LANGUAGE OverloadedStrings, CPP #-}
 
-module Server (
-    server
-  , defaultDomain
-  , defaultPort
-  , Service(..)
-  ) where
+module Server (server, defaultDomain, defaultPort) where
 
 import Control.Applicative ((<$>))
-import Control.Concurrent (forkIO)
+import Control.Concurrent (forkIO, threadDelay)
 import Control.Exception (try)
-import Control.Monad (void)
+import Control.Monad (void, unless, when)
 import qualified Data.ByteString.Char8 as BS (pack)
 import Network (Socket, sClose)
 import Network.HTTP.Date (formatHTTPDate, epochTimeToHTTPDate)
 import Network.Wai.Application.Classic hiding ((</>), (+++))
 import Network.Wai.Handler.Warp
-import System.Date.Cache (DateCacheConf(..), clockDateCacher)
-import System.Exit (ExitCode(..))
+import System.Exit (ExitCode(..), exitSuccess)
+import System.IO
 import System.IO.Error (ioeGetErrorString)
 import System.Posix (exitImmediately, Handler(..), epochTime)
+import System.Posix (getProcessID, setFileMode)
 #ifdef REV_PROXY
 import qualified Network.HTTP.Conduit as H
 #endif
@@ -43,6 +39,18 @@ defaultDomain = "localhost"
 defaultPort :: Int
 defaultPort = 80
 
+backlogNumber :: Int
+backlogNumber = 2048
+
+openFileNumber :: Integer
+openFileNumber = 10000
+
+oneSecond :: Int
+oneSecond = 1000000
+
+logBufferSize :: Int
+logBufferSize = 4 * 1024 * 10
+
 ----------------------------------------------------------------
 
 #ifdef REV_PROXY
@@ -53,40 +61,70 @@ type ConnPool = ()
 
 ----------------------------------------------------------------
 
-server :: Option -> RouteDB -> Service -> Reporter -> Stater -> Logger -> IO ()
-server opt route service rpt stt lgr = reportDo rpt $ do
+server :: Option -> RouteDB -> Reporter -> IO ()
+server opt route rpt = reportDo rpt $ do
+    unlimit openFileNumber
+    svc <- openService opt
+    unless debug writePidFile
+    logCheck logtype
+    stt <- initStater
+    (lgr,flusher,updater) <- initLogger FromSocket logtype -- FIXME
+    -- killed by signal
     setGroupUser (opt_user opt) (opt_group opt) -- don't change the user of the master process
-    getInfo <- fileCacheInit
-    setHandler sigStop   stopHandler
-    setHandler sigRetire retireHandler
-    setHandler sigInfo   infoHandler
+    (getInfo, _fixme) <- fileCacheInit
 #ifdef REV_PROXY
     mgr <- H.newManager H.def { H.managerConnCount = 1024 } -- FIXME
 #else
     let mgr = ()
 #endif
-    setHandler sigReload (reloadHandler lgr getInfo mgr)
+    setHandlers opt rpt svc stt lgr getInfo mgr
     report rpt "Worker Mighty started"
-    reload opt route service rpt stt lgr getInfo mgr
+    void . forkIO $ reload opt route svc rpt stt lgr getInfo mgr -- FIXME
+    mainLoop rpt stt flusher
+  where
+    debug = opt_debug_mode opt
+    pidfile = opt_pid_file opt
+    writePidFile = do
+        pid <- getProcessID
+        writeFile pidfile $ show pid ++ "\n"
+        setFileMode pidfile 0o644
+    logspec = FileLogSpec {
+        log_file          = opt_log_file opt
+      , log_file_size     = fromIntegral $ opt_log_file_size opt
+      , log_backup_number = opt_log_backup_number opt
+      }
+    logtype
+      | not (opt_logging opt) = LogNone
+      | debug                 = LogStdout logBufferSize
+      | otherwise             = LogFile logspec logBufferSize
+
+setHandlers :: Option -> Reporter -> Service -> Stater -> ApacheLogger -> (Path -> IO FileInfo) -> ConnPool -> IO ()
+setHandlers opt rpt svc stt lgr getInfo mgr = do
+    setHandler sigStop   stopHandler
+    setHandler sigRetire retireHandler
+    setHandler sigInfo   infoHandler
+    setHandler sigReload reloadHandler
   where
     stopHandler = Catch $ do
         report rpt "Worker Mighty finished"
         finReporter rpt
-        finLogger lgr
-        closeService service
+--        finLogger lgr -- FIXME
+        closeService svc
         exitImmediately ExitSuccess
     retireHandler = Catch $ ifWarpThreadsAreActive stt $ do
         report rpt "Worker Mighty retiring"
-        closeService service
+        closeService svc
         goRetiring stt
-    reloadHandler lggr getInfo mgr = Catch $ ifWarpThreadsAreActive stt $
-        ifRouteFileIsValid rpt opt $ \newroute -> do
-            report rpt "Worker Mighty reloaded"
-            void . forkIO $ reload opt newroute service rpt stt lggr getInfo mgr
     infoHandler = Catch $ do
         i <- bshow <$> getConnectionCounter stt
         status <- bshow <$> getServerStatus stt
         report rpt $ status +++ ": # of connections = " +++ i
+    reloadHandler = Catch $ ifWarpThreadsAreActive stt $
+        ifRouteFileIsValid rpt opt $ \newroute -> do
+            report rpt "Worker Mighty reloaded"
+            void . forkIO $ reload opt newroute svc rpt stt lgr getInfo mgr
+
+----------------------------------------------------------------
 
 ifRouteFileIsValid :: Reporter -> Option -> (RouteDB -> IO ()) -> IO ()
 ifRouteFileIsValid rpt opt act = case opt_routing_file opt of
@@ -98,18 +136,18 @@ ifRouteFileIsValid rpt opt act = case opt_routing_file opt of
 ----------------------------------------------------------------
 
 reload :: Option -> RouteDB -> Service
-       -> Reporter -> Stater -> Logger
+       -> Reporter -> Stater -> ApacheLogger
        -> (Path -> IO FileInfo) -> ConnPool
        -> IO ()
-reload opt route service rpt stt lgr getInfo _mgr = reportDo rpt $ do
+reload opt route svc rpt stt lgr getInfo _mgr = reportDo rpt $ do
     setMyWarpThreadId stt
-    zdater <- initZoneDater
+    zdater <- initZoneDater -- FIXME
 #ifdef REV_PROXY
     let app req = fileCgiApp (cspec zdater) filespec cgispec revproxyspec route req
 #else
     let app req = fileCgiApp (cspec zdater) filespec cgispec route req
 #endif
-    case service of
+    case svc of
         HttpOnly s  -> runSettingsSocket setting s app
 #ifdef TLS
         HttpsOnly s -> runTLSSocket tlsSetting setting s app
@@ -135,7 +173,7 @@ reload opt route service rpt stt lgr getInfo _mgr = reportDo rpt $ do
     serverName = BS.pack $ opt_server_name opt
     cspec zdater = ClassicAppSpec {
         softwareName = serverName
-      , logger = apatcheLogger lgr
+      , logger = lgr
       , dater = zdater
       , statusFileDir = fromString $ opt_status_file_dir opt
       }
@@ -165,7 +203,51 @@ reload opt route service rpt stt lgr getInfo _mgr = reportDo rpt $ do
 
 ----------------------------------------------------------------
 
+openService :: Option -> IO Service
+openService opt
+  | service == 1 = do
+      s <- listenSocket httpsPort backlogNumber
+      debugMessage $ "HTTP/TLS service on port " ++ httpsPort ++ "."
+      return $ HttpsOnly s
+  | service == 2 = do
+      s1 <- listenSocket httpPort backlogNumber
+      s2 <- listenSocket httpsPort backlogNumber
+      debugMessage $ "HTTP service on port " ++ httpPort ++ " and "
+                  ++ "HTTP/TLS service on port " ++ httpsPort ++ "."
+      return $ HttpAndHttps s1 s2
+  | otherwise = do
+      s <- listenSocket httpPort backlogNumber
+      debugMessage $ "HTTP service on port " ++ httpPort ++ "."
+      return $ HttpOnly s
+  where
+    httpPort  = show $ opt_port opt
+    httpsPort = show $ opt_tls_port opt
+    service = opt_service opt
+    debug = opt_debug_mode opt
+    debugMessage msg = when debug $ do
+        putStrLn msg
+        hFlush stdout
+
+----------------------------------------------------------------
+
 closeService :: Service -> IO ()
 closeService (HttpOnly s) = sClose s
 closeService (HttpsOnly s) = sClose s
 closeService (HttpAndHttps s1 s2) = sClose s1 >> sClose s2
+
+
+----------------------------------------------------------------
+
+-- FIXME log controller should be implemented here
+mainLoop :: Reporter -> Stater -> LogFlusher -> IO ()
+mainLoop rpt stt flusher = do
+    threadDelay oneSecond
+    retiring <- isRetiring stt
+    counter <- getConnectionCounter stt
+    if retiring && counter == 0 then do
+        report rpt "Worker Mighty retired"
+        finReporter rpt
+        flusher
+        exitSuccess
+      else
+        mainLoop rpt stt flusher
