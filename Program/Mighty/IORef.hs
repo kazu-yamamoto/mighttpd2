8444fd795deda603839779193d29e9d20df4aa3a
Kazu Yamamoto <kazu@iij.ad.jp>
2013-11-22 15:41:09 +0900

    changing strictAtomicModifyIORef according to P284 of the ParCon book.

--- Program/Mighty/IORef.hs
+++ Program/Mighty/IORef.hs
@@ -6,10 +6,15 @@ import Data.IORef
 
 ----------------------------------------------------------------
 
--- | Strict version of 'atomicModifyIORef'
---   The returned value is strictly evaluated and thrown away.
---   This prevents space leak.
+-- | Strict version of 'atomicModifyIORef'.
+--   When modifying the IORef, calculation is delayed.
+--   So, modification is quick and CAS would success.
+--   After modification, calculation is forced.
+--   So, no space leak.
 strictAtomicModifyIORef :: IORef a -> (a -> a) -> IO ()
-strictAtomicModifyIORef ref f = do
-    !_ <- atomicModifyIORef ref (\x -> let !r = f x in (r, ()))
-    return ()
+strictAtomicModifyIORef ref f =  do
+    c <- atomicModifyIORef ref
+            (\x -> let a = f x          -- Lazy application of "f"
+                    in (a, a `seq` ())) -- Lazy application of "seq"
+    -- The following forces "a `seq` ()", so it also forces "f x".
+    c `seq` return c
