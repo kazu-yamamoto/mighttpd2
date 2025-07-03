c538b50827e5c7a12ff3024baef5fe1bbcc287fe
Kazu Yamamoto <kazu@iij.ad.jp>
2025-07-03 13:01:13 +0900

    renaming

--- utils/restart.sh
+++ utils/restart.sh
@@ -4,7 +4,7 @@
 # Assuming this directory contains everything.
 MIGHTY_PATH=/usr/local/mighty
 
-# sudo certbot renew --webroot -w /usr/local/mighty/webroot --deploy-hook /usr/local/mighty/renew-cert.sh
+# sudo certbot renew --webroot -w /usr/local/mighty/webroot --deploy-hook /usr/local/mighty/restart.sh
 
 ${MIGHTY_PATH}/mightyctl retire
 ${MIGHTY_PATH}/mighty ${MIGHTY_PATH}/conf ${MIGHTY_PATH}/route +RTS -N2
