89c39a35a6aa3bdcc7e7760b88420b4d24002217
Kazu Yamamoto <kazu@iij.ad.jp>
2025-07-03 13:06:59 +0900

    comments for restart.sh

--- utils/restart.sh
+++ utils/restart.sh
@@ -1,9 +1,20 @@
 #! /bin/sh
-# A shell script for renewing certificates of Let's encrypt.
+
+# A shell script to restart mighty:
+# * The current mighty is retired
+# * A new mighty is started
 
 # Assuming this directory contains everything.
 MIGHTY_PATH=/usr/local/mighty
+# % ls -1 /usr/local/mighty
+# conf
+# mighty*
+# mightyctl*
+# restart.sh*
+# route
+# webroot/
 
+# This script can be used with "certbot renew"
 # sudo certbot renew --webroot -w /usr/local/mighty/webroot --deploy-hook /usr/local/mighty/restart.sh
 
 ${MIGHTY_PATH}/mightyctl retire
